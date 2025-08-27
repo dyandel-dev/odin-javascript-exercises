34fcaa799aaa9d0cd9767c3cd8b2438d2aa31820
JD <117078422+dyandel-dev@users.noreply.github.com>
2025-08-27 19:34:59 +0800

    Add repeatString function

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (text, num) {
+  let repeated = "";
 
+  if (num < 0) {
+    return "ERROR";
+  }
+
+  for (let i = 0; i < num; i++) {
+    repeated += text;
+  }
+
+  return repeated;
 };
 
 // Do not edit below this line
