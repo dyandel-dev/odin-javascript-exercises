c62617d437ca1fec832935001a181a270988b71e
JD <117078422+dyandel-dev@users.noreply.github.com>
2025-08-28 19:29:51 +0800

    Add sumAll function

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,31 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  let sum = 0;
 
+  if (
+    !Number.isInteger(a, b) ||
+    a < 0 ||
+    b < 0 ||
+    typeof a === "string" ||
+    typeof b === "string" ||
+    Array.isArray(a) ||
+    Array.isArray(b)
+  ) {
+    return "ERROR";
+  } else {
+    if (a > b) {
+      for (let i = a; i >= b; i--) {
+        sum += i;
+      }
+    } else {
+      for (let i = a; i <= b; i++) {
+        sum += i;
+      }
+    }
+    return sum;
+  }
 };
+
+console.log(sumAll(1, "10"));
 
 // Do not edit below this line
 module.exports = sumAll;
