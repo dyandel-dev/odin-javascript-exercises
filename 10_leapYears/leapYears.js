9f08708b3b442d453a71f330a40f10e71966dadf
JD <117078422+dyandel-dev@users.noreply.github.com>
2025-08-28 20:16:57 +0800

    Add leapYears function

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,18 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  if (year % 4 === 0) {
+    if (year % 100 === 0 && year % 400 === 0) {
+      return true;
+    } else if (year % 100 === 0 && !year % 400 === 0) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 
+  return false;
 };
+
+console.log(leapYears(2000));
 
 // Do not edit below this line
 module.exports = leapYears;
