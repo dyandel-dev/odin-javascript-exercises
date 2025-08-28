98a0c59b22994510254cc829981e85baedcae0ed
JD <117078422+dyandel-dev@users.noreply.github.com>
2025-08-28 18:45:05 +0800

    Add removeFromArray function

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+  const newArr = [];
+
+  arr.forEach((item) => {
+    if (!args.includes(item)) {
+      newArr.push(item);
+    }
+  });
+
+  return newArr;
 };
+
+console.log(removeFromArray([1, 2, 2, 3], 2));
 
 // Do not edit below this line
 module.exports = removeFromArray;
