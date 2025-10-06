23cbad0ecf33c8984f4bb761f846a0c9f92e980e
JD <117078422+dyandel-dev@users.noreply.github.com>
2025-10-06 16:40:20 +0800

    Add functionality to check for palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (text) {
+  const remove_char = text.replace(/[.,!\s]/g, "").toLowerCase();
 
+  const reverse_string = remove_char.split("").reverse().join("");
+
+  return remove_char === reverse_string;
 };
 
 // Do not edit below this line
