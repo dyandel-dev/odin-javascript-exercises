1eaa3d496307464f1692f4e5ca2d10759190ae2d
JD <117078422+dyandel-dev@users.noreply.github.com>
2025-10-07 15:40:39 +0800

    Create and implement getTheTitles function

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function (arr) {
+  let new_arr = [];
 
+  for (let i = 0; i < arr.length; i++) {
+    new_arr.push(arr[i].title);
+  }
+
+  return new_arr;
 };
 
 // Do not edit below this line
